c036abb6de5c52ad9830ccd663482df85d8f43d4
Ngozi Aguluka <>
2024-11-10 16:23:45 +0100

    fix icon hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ const Button = ({ children, variant, className, ...props }: ButtonProps) => {
         variant === 'primary'
           ? 'bg-green-800 text-schemes-primary-container hover:opacity-80 py-3 px-4 rounded-xl'
           : variant === 'icon'
-          ? 'max-w-12 h-auto xl:max-w-16 aspect-square rounded-full bg-schemes-surface-container-low flex-shrink-0'
+          ? 'max-w-12 h-auto xl:max-w-16 aspect-square rounded-full bg-schemes-surface-container-low hover:bg-schemes-surface-container flex-shrink-0'
           : 'border border-schemes-outline hover:bg-schemes-surface-container  py-3 px-4 rounded-xl',
         'w-full flex items-center justify-center gap-2 transition-all duration-300 ease-in-out',
         className as string
